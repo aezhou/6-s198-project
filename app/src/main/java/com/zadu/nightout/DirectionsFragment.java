c86898244c513e65a28ff99f1e592aa427e38b8f
Tricia Divita <tdivita@mit.edu>
2015-04-21 05:19:19 -0400

    Completed basic layout for directions tab.

--- app/src/main/java/com/zadu/nightout/DirectionsFragment.java
+++ app/src/main/java/com/zadu/nightout/DirectionsFragment.java
@@ -7,6 +7,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 
 /**
@@ -22,6 +24,8 @@ public class DirectionsFragment extends Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+    // Create a place to store the destination spinner
+    private Spinner destSpinner;
 
     // TODO: Rename and change types of parameters
     private String mParam1;
@@ -64,7 +68,16 @@ public class DirectionsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_directions, container, false);
+        View v = inflater.inflate(R.layout.fragment_directions, container, false);
+
+        // Populate the destination spinner
+        destSpinner = (Spinner) v.findViewById(R.id.dest_spinner);
+        ArrayAdapter adapter = ArrayAdapter.createFromResource(getActivity(),
+                R.array.dest_spinner_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        destSpinner.setAdapter(adapter);
+
+        return v;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
